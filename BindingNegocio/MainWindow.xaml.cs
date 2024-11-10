8151ecde404c649744b5c95d38e1eaed80dbeb2a
MANUEL MADRIGAL LOSA <m.madrigallosa@edu.gva.es>
2024-11-10 19:08:03 +0100

    Agregar archivos de proyecto.

---
+++ BindingNegocio/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BindingNegocio
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Persona usuario = new Persona("Juan", 20);
+
+            /*
+            // Forma-1 para definir el DataContext por propiedades.
+            nombreTextBox.DataContext = usuario;
+            edadTextBox.DataContext = usuario;
+            */
+
+            // Forma-2 para definir el DataContext por PANEL.
+            datosStackPanel.DataContext = usuario;  
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            // MessageBox.Show("Hola " + nombreTextBox.Text + ", tienes " + edadTextBox.Text + " años.");  
+            // MessageBox.Show(((Persona)nombreTextBox.DataContext).Nombre);
+
+            ((Persona)nombreTextBox.DataContext).Nombre = "Carlos";
+        }
+    }
+}
---
+++ BindingNegocio/Persona.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingNegocio
+{
+    // Definir interfaz INotifyPropertyChanged
+    public class Persona:INotifyPropertyChanged
+    {
+        // public string Nombre { get; set; }
+        // public int Edad { get; set; }
+
+        private string _nombre;
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { 
+                _nombre = value; 
+
+                // this.NotifyPropertyChanged("Nombre");
+                NotifyPropertyChanged("Nombre");
+            }
+        }
+
+        private int _edad;
+
+        public int Edad
+        {
+            get { return _edad; }
+            set {
+                _edad = value; 
+                NotifyPropertyChanged("Edad");
+            }
+        }
+
+        public Persona()
+        {
+            Nombre = "";
+            Edad = 0;   
+        }
+
+        public Persona(string nombre, int edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+        }
+
+        // Declarar el evento.
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        // Declarar método para invocar el evento.
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }   
+    }
+}
